4a2c591b7e47b08c02f42746083731c9e40bf3cc
Hao Liu <hliu@redhat.com>
2015-07-28 14:06:55 +0800

    test: Move all tests from setup.py to tox
    Signed-off-by: Hao Liu <hliu@redhat.com>

--- setup.py
+++ setup.py
@@ -1,43 +1,11 @@
 #!/usr/bin/env python
 
 import io
-import logging
 import os
-import subprocess
 
 from setuptools import setup
-from setuptools.command.test import test
 
 import dice
-
-
-class SelfTest(test):
-    def run(self):
-        fail = False
-        cmds = [
-            'coverage run -m unittest discover tests -p *unittest.py'.split(),
-            'pylint dice/ scripts/dice --reports=n --disable=R,C,I'.split(),
-            'pep8 scripts/dice dice/ --ignore=E501'.split(),
-        ]
-        for cmd in cmds:
-            try:
-                subprocess.check_call(cmd)
-            except subprocess.CalledProcessError:
-                fail = True
-                logging.error('Error when running test cmd: %s', ' '.join(cmd))
-
-        if fail:
-            exit(1)
-
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
 
 
 def get_config_dir():
@@ -73,8 +41,4 @@ setup(
     scripts=['scripts/dice'],
     packages=get_packages(),
     data_files=get_data_files(),
-    cmdclass={
-        'test': SelfTest,
-        'check': SelfTest,
-    },
 )
